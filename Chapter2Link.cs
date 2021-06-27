dd8f9bd53f9eaec553822b472f738f3165b5d067
Kouichi Umezawa <kouichiume@gmail.com>
2021-06-27 16:46:03 +0900

    chapter2 1

---
+++ Chapter2Link.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Chapter2Link<T> where T : new ()
+
+    {
+
+        public T getNewItem()
+        {
+            return new T();
+        }
+
+        public static void deleteDuplicatedNode(LinkedList<T> ll ,T v)
+        {
+
+            
+
+            LinkedListNode < T > n = null;
+            do
+            {
+                n = ll.Find(v);
+
+                if (n != null)
+                {
+                    ll.Remove(n);
+                }
+
+            }
+            while (n != null);
+
+
+            return;
+
+        }
+
+
+
+    }
+}
