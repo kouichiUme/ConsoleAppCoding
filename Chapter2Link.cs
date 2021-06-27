41f2a2263ea9ee67b198394eb985f9827b7f7264
Kouichi Umezawa <kouichiume@gmail.com>
2021-06-27 17:45:04 +0900

    chapter 2  消込と終わりから３つめ

--- Chapter2Link.cs
+++ Chapter2Link.cs
@@ -4,9 +4,71 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    public class Chapter2Link<T> where T : new ()
+    public class Chapter2Link<T> where T : new()
+
+
 
     {
+        class Node<T> where T : new()
+        {
+            public T t;
+            public Node<T> next;
+
+        }
+
+        private Node<T> node;
+
+        public Chapter2Link(){
+            node = new Node<T>();
+        }
+
+
+
+
+        public void add(T a)
+        {
+            Node<T> node = new Node<T>();
+            node.t = a;
+
+            Node<T> end;
+            for (end = this.node;end.next !=null;end = end.next)
+            {
+                // なにもしない
+            }
+            
+            end.next = node;
+        }
+
+        public T find3rdNode()
+        {
+            Node<T> d = new Node<T>();
+            Node<T> second = new Node<T>();
+            Node<T> end;
+            int count = 0;
+            for(end = node; end.next != null; end = end.next)
+            {
+                if (count > 2)
+                {
+                    d = second ;
+
+                }
+
+
+                if (count > 1)
+                {
+                    
+                    second = end;
+                    
+                }
+                count++;
+
+            }
+
+            return d.t;
+
+
+        }
+
 
         public T getNewItem()
         {
