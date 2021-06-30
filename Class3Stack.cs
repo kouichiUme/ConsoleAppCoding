ac9291ac57f6b93f5d83429606992d65c084ba55
Kouichi Umezawa <kouichiume@gmail.com>
2021-06-30 23:58:11 +0900

    chapter3 練習用

---
+++ Class3Stack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class3Stack
+    {
+
+        public void protoVanEldeBoas()
+
+
+        {
+
+
+
+        }
+
+    }
+}
