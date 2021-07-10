ca338bb3059106a6a6509abdd6fb1bf4a655416d
Kouichi Umezawa <kouichiume@gmail.com>
2021-07-11 02:08:13 +0900

    stack の問題

--- Class3Stack.cs
+++ Class3Stack.cs
@@ -1,44 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ConsoleApp1;
 
 namespace ConsoleApp1
 {
+
+    class NodeOfThree<T> where T : new()
+    {
+        public T t;
+        public int no;
+
+        public NodeOfThree(T i, int v)
+        {
+            this.t = i;
+            this.no = v;
+        }
+    }
+
     class Class3Stack
     {
+        private LinkedList<NodeOfThree<int>> node;
 
-        public void protoVanEldeBoas()
-
-
+        public void initThreeStacksInARow()
         {
-
-
-
+            this.node = new LinkedList<NodeOfThree<int>>();
 
 
         }
 
-        public void vebTreeMember()
+        public void addFirst(int i)
         {
+            node.AddLast(new NodeOfThree<int>(i, 1));
 
         }
 
-        public void vebTreeSuccessor()
+        public void addSecond(int i)
         {
+            node.AddLast(new NodeOfThree<int>(i, 2));
+        }
+
+        public void addThird(int i)
+        {
+            node.AddLast(new NodeOfThree<int>(i, 3));
+        }
+
+    }
+
+    class Class3Problem2Stack<T > where T : IComparable
+    {
+
+        private LinkedList<T> listStack = null;
+        private T min ;
+
+
+
+        public void add(T t)
+        {
+            listStack.AddLast(t);
+            if(t.CompareTo(min) < 0)
+            {
+                min = t;
+            }
 
         }
 
-
-        public void vebTreePredecessor()
+        // これだとO(1)ではない
+        public T pop()
         {
+            T t = listStack.Last.Value;
+            
+            // 最後のノードが最小値だった場合
+            if (t.CompareTo(min) ==0 )
+            {
+                T i = listStack.First.Value;
+                foreach(T x in listStack)
+                {
+                    if (x.CompareTo(i) < 0)
+                    {
+                        min = x;
+                        
+                    }
 
+                }
+
+
+            }
+
+
+           return t;
         }
 
-        public void disjointSet()
+        // O(1) table-key/value or static
+        // min 
+        public T getMin()
         {
 
+            return this.min;
         }
+
 
 
     }
+
+
 }
+
+    
---
+++ VanEldeBoasTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class VanEldeBoasTree
+    {
+        public void protoVanEldeBoas()
+
+
+        {
+
+
+
+
+
+        }
+
+        public void vebTreeMember()
+        {
+
+        }
+
+        public void vebTreeSuccessor()
+        {
+
+        }
+
+
+        public void vebTreePredecessor()
+        {
+
+        }
+
+        public void disjointSet()
+        {
+
+        }
+
+
+    }
+}
