7a97a33d8d733d63fe7db405aedaf810dc4eff2d
Kouichi Umezawa <kouichiume@gmail.com>
2021-08-01 19:56:52 +0900

    reveiw al

--- Chapter5Bit.cs
+++ Chapter5Bit.cs
@@ -8,12 +8,16 @@ namespace ConsoleApp1
     {
         public UInt32 problem1(UInt32 binaryN, UInt32 binaryM,int i,int j)
         {
+            // self xor make all 0;
+
+            // binaryM ^= binaryM;
 
             UInt32 binaryIJ = 0b1;
             UInt32 binaryFilter = 0b0;
             for(int k = 0; k < j; k++)
             {
-                binaryIJ <<= j;
+                binaryIJ = 0b1;
+                binaryIJ <<= k;
                 binaryFilter += binaryIJ;
 
             }
@@ -21,7 +25,7 @@ namespace ConsoleApp1
 
             binaryFilter = ~binaryFilter;
             UInt32 tmpBinary = binaryN & binaryFilter;
-
+            binaryM <<= i;
             tmpBinary |= binaryM;
 
             return tmpBinary;
