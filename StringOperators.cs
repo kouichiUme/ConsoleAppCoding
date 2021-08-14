b38f410cc5a67dbc04d410a1b5882e72a4f1a455
Kouichi Umezawa <kouichiume@gmail.com>
2021-08-14 23:34:02 +0900

    stringmatcher

---
+++ StringOperators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class StringOperators
+    {
+
+
+        public void nativeStringMatcher()
+        {
+
+        }
+    }
+}
