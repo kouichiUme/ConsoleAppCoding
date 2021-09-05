21ab4c4523eda38391ef758a73fb765d09da6c63
Kouichi Umezawa <kouichiume@gmail.com>
2021-09-06 02:58:37 +0900

    delegate and event

--- MergeSort.cs
+++ MergeSort.cs
@@ -7,12 +7,52 @@ namespace ConsoleApp1
     public class MergeSort
     
     {
-        public static void getFirstNode()
+
+
+        public delegate string delegateBroker(string a,string b);
+
+        public event delegateBroker messageBusBrokerEvent;
+
+
+        public void getFirstNode()
         {
 
+            delegateBroker d;
+
+            try
+            {
+                checked
+                {
+
+                }
+            }
+            catch(OverflowException ofException)
+            {
+                // 
+
+            }
+
+            // delegate
+            d = delegate (string a, string b)
+            {
+
+                return a;
+            };
+
+            messageBusBrokerEvent = delegate(string a,string b)
+            {
+                return "event ";
+            };
+
+            string a="a", b="b";
+            messageBusBrokerEvent(a,b);
 
 
         }
+
+
+
+
     }
 
 
