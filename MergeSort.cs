2778bf323fbb396ffb0324e290c9da50d26c45ad
Kouichi Umezawa <kouichiume@gmail.com>
2021-09-06 03:15:55 +0900

    デフォルトコンストラクトがある参照型T と値型 V

--- MergeSort.cs
+++ MergeSort.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    public class MergeSort
-    
+    public class MergeSort<T,V> 
+        where T :  class,new()
+        where V : struct
+  
     {
 
 
