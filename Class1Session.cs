08332f54044da432f79dd0eb735894b6981c36d1
Kouichi Umezawa <kouichiume@gmail.com>
2021-10-06 02:32:24 +0900

    とりあえず

---
+++ Class1Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Class1Session
+    {
+
+
+        public  Class1Session(){
+            　
+        }
+    }
+}
