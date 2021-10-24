95e0c38bc186bebed0ebe43e17c9a9277e812dbc
Kouichi Umezawa <kouichiume@gmail.com>
2021-10-24 22:18:09 +0900

    sql

--- Chapter14Sql.cs
+++ Chapter14Sql.cs
@@ -8,6 +8,30 @@ namespace ConsoleApp1
     {
 
 
+        public static void p1()
+        {
+
+            "select t.TenantName from Tenants t";
+            "left join AptTenants as at";
+            "on at.TenantID = t.TenantID";
+            "group by t.TenantName";
+            "having count(at.AptID) > 1";
+
+
+        }
+
+
+        public static void p2()
+        {
+            "select b.BuildingID ,count(r.Status) from Buildings b";
+            "left join Apartments a";
+            "on b.BuildingID= a.BuildingID";
+            "left join  Requests r";
+            "on r.AptID = a.AptID";
+            "where r.Status = 'open'";
+            "group by b.BuildingID";
+        }
+
         public static void JoinImplicit()
         {
 
