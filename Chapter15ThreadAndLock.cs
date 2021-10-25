86146801f9d9862d92ee90a45162ec8d86958350
Kouichi Umezawa <kouichiume@gmail.com>
2021-10-25 22:58:02 +0900

    thread

--- Chapter15ThreadAndLock.cs
+++ Chapter15ThreadAndLock.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleApp1
 {
-    public class Chapter15ThreadAndLock
+    public class Chapter15ThreadAndLock 
     {
 
+        public static void sread()
+        {
 
-        
+        }
+
+        public static void sreadCaller()
+        {
+
+            Thread t = new Thread(new ThreadStart(sread));
+
+
+
+        }
+
     }
 }
