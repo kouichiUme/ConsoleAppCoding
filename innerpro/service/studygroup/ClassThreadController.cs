404713bae1ee17bb099fb4330129802cbb8c36bc
Kouichi Umezawa <kouichiume@gmail.com>
2021-11-06 14:03:53 +0900

    architecture package design

---
+++ innerpro/service/studygroup/ClassThreadController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace innerpro.service.studygroup
+{
+    public class ClassThreadController
+    {
+    }
+}
