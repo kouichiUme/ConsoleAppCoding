ddf1df4a2fb234669c6d8650064e69067f9d52a9
Kouichi Umezawa <kouichiume@gmail.com>
2021-11-21 14:24:50 +0900

    勉強会準備 中級問題２

--- Chapter16Intermediate.cs
+++ Chapter16Intermediate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
@@ -18,8 +19,23 @@ namespace ConsoleApp1
 
 
         // 
-        public void newFile()
+        public void swapWithoutVariables()
         {
+
+        }
+
+        /// <summary>
+        /// 本の中にある単語が何度出てくるか調べる
+        /// </summary>
+        /// <param name="book"></param>
+        public static void countWordInBook(string book,string word)
+        {
+
+            // 正規表現で 文字列の中に何度出現するかしらべる
+            MatchCollection matched = Regex.Matches(book, word);
+
+            System.Console.WriteLine(matched.Count);
+
 
         }
 
