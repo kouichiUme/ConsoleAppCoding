3114d7a2170ca50dd1682c57518d86ce895e86b1
Kouichi Umezawa <kouichiume@gmail.com>
2021-12-15 21:19:22 +0900

    numpy and pandas with C#

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -6,7 +6,6 @@ namespace ClassLibrary1
     
     public class Class1
     {
-        private log4net = new Microsoft.Extensions.Logging();
 
         public void ServiceMethod()
         {
---
+++ Num/NumPyService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp;
+
+namespace ConsoleApp1.Num
+{
+    public class NumPyService
+    {
+
+        public static NDArray initNDArray()
+        {
+
+            NDArray nd = np.full(5,12);
+
+            nd.reshape(3, 4);
+
+            return nd;
+        }
+
+    }
+}
